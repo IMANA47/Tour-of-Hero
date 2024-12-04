fe017ed9bae63b8f8cc0d500bdd181df9669b75b
Nsengimana François <francknsengimana@gmail.com>
2024-12-04 16:16:49 +0100

    correction final

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Aminadabou' },
+  { id: 13, name: 'Pablo' },
+  { id: 14, name: 'Christine' },
+  { id: 15, name: 'Nakuze' },
+  { id: 16, name: 'Anitha' },
+  { id: 17, name: 'Divine' },
+  { id: 18, name: 'Ngatoya' },
+  { id: 19, name: 'Kongo' },
+  { id: 20, name: 'Sylvain' }
+];
